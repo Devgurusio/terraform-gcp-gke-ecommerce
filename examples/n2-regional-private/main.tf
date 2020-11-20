6dd0e6faedabb34bff2adc4e3a76a5b999d0f692
Lucas Giacomazzi <lucasgiacomazzi@gmail.com>
2020-11-20 18:10:29 -0300

    Removed old examples

--- examples/n2-regional-private/main.tf
+++
@@ -1,9 +0,0 @@
-module "gke" {
-  source     = "../.."
-  project_id = "project-gaia-101"
-  region     = "us-central1"
-  zones      = ["us-central1-a", "us-central1-c", "us-central1-f"]
-
-  gke_instance_type   = "n2-standard-2"
-  cluster_name_suffix = "-n2-regional"
-}
