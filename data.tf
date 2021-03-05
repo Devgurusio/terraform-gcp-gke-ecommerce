43759ca29121c00d30466c3dca108dc0fba5df94
Jose Antonio Hernàndez Martínez <jose@devgurus.io>
2021-03-05 09:43:50 +0100

    Fix zonal zones by removing the option to create multi-zonal cluster (1 zonal master - n nodes)

--- data.tf
+++ data.tf
@@ -4,8 +4,8 @@ locals {
   region     = var.region == null ? join("-", slice(split("-", var.zones[0]), 0, 2)) : var.region
   zone_count = length(var.zones)
 
-  // for regional cluster - use var.zones if provided, use available otherwise, for zonal cluster use var.zones with first element extracted
-  node_locations = var.regional ? coalescelist(compact(var.zones), sort(random_shuffle.available_zones.result)) : slice(var.zones, 1, length(var.zones))
+  // Modified to only create regional or zonal clusters (multi-zonal option removed from template)
+  node_locations = var.regional ? coalescelist(compact(var.zones), sort(random_shuffle.available_zones.result)) : null
 
   // Kubernetes version
   min_master_version_regional = var.min_kubernetes_version != "latest" ? var.min_kubernetes_version : data.google_container_engine_versions.region.latest_master_version
--- nodes.tf
+++ nodes.tf
@@ -8,6 +8,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
   project        = var.project_id
   location       = local.location
+  // if it is a regional cluster set all the zones, if it is zonal it will be null, no additional zones (multi-zonal)
   node_locations = local.node_locations
 
   node_config {
